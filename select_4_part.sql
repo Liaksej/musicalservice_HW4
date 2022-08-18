aa203109322842d1851e23e764324d5021d7f2aa
Liaksej <tarusov.liaksej@gmail.com>
2022-08-18 11:40:00 +0200

    Corrected version taking into consideration the expert's advice from 18.08.2022

--- select_4_part.sql
+++ select_4_part.sql
@@ -15,24 +15,26 @@ SELECT album_name, AVG(length)
   JOIN tracks t ON a.album_id = t.album_id
  GROUP BY album_name;
 
--- Запрос всех исполнителей, которые не выпустили альбомы в 2020 году:
-SELECT name_or_alias
+-- Запрос всех исполнителей, которые не выпустили альбомы в 2014 году:
+SELECT DISTINCT name_or_alias
   FROM albums a
   JOIN albums_artists aa ON a.album_id = aa.album_id
   JOIN artists a2 ON aa.artist_id = a2.artist_id
- WHERE release_year != 2020
- GROUP BY name_or_alias;
+ WHERE name_or_alias != (SELECT DISTINCT name_or_alias
+                           FROM albums a
+                           JOIN albums_artists aa ON a.album_id = aa.album_id
+                           JOIN artists a2 ON aa.artist_id = a2.artist_id
+                          WHERE release_year = 2014);
 
 -- Запрос названия сборников, в которых присутствует конкретный исполнитель:
-SELECT compilation_name
+SELECT DISTINCT compilation_name
   FROM artists a
   JOIN albums_artists aa ON a.artist_id = aa.artist_id
   JOIN albums a2 ON aa.album_id = a2.album_id
   JOIN tracks t ON a2.album_id = t.album_id
   JOIN tracks_compilations tc ON t.track_id = tc.track_id
   JOIN compilations c ON tc.compilation_id = c.compilation_id
- WHERE name_or_alias = 'Arlo Parks'
- GROUP BY compilation_name;
+ WHERE name_or_alias = 'Arlo Parks';
 
 -- Запрос названия альбомов, в которых присутствуют исполнители более 1 жанра:
 SELECT album_name, COUNT(*)
@@ -50,13 +52,12 @@ SELECT track_name
  WHERE compilation_id IS NULL;
 
 -- Запрос исполнителя(-ей), написавшего самый короткий по продолжительности трек:
-SELECT name_or_alias
+SELECT DISTINCT name_or_alias
   FROM tracks t
   JOIN albums a ON t.album_id = a.album_id
   JOIN albums_artists aa ON a.album_id = aa.album_id
   JOIN artists a2 ON aa.artist_id = a2.artist_id
- WHERE length = (SELECT MIN(length) FROM tracks)
- GROUP BY name_or_alias;
+ WHERE length = (SELECT MIN(length) FROM tracks);
 
 -- Запрос названия альбомов, содержащих наименьшее количество треков:
 
